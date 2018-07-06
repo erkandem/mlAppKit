b7157773a823c9504401a126db6d7dd6d270d974
kan <kan@DESKTOP-N9B01HV.mshome.net>
2018-07-06 12:52:26 +0200

    changed names

--- functions/conversion_launcher/conversion_launcher.m
+++ functions/conversion_launcher/conversion_launcher.m
@@ -2,6 +2,7 @@ function conversion_launcher()
 mlapp2classdef_edit(fullfile('launch','icbm.mlapp'),'ReplaceAppUI',true);
 mlapp2classdef_edit(fullfile('launch','scud.mlapp'),'ReplaceAppUI',true);
 mlapp2classdef_edit(fullfile('launch','stinger.mlapp'),'ReplaceAppUI',true);
+mlapp2classdef_edit(fullfile('popups','popup_about.mlapp'),'ReplaceAppUI',false);
 mlapp2classdef_edit(fullfile('popups','popup_settings.mlapp'),'ReplaceAppUI',false);
 main_extractor();
 end
