8131ba00ce2c320097cea12463e39a3cec91b7cb
kan <kan@DESKTOP-N9B01HV.mshome.net>
2018-07-06 11:12:41 +0200

    debug app_launch

--- functions/app_launch/app_launch.m
+++ functions/app_launch/app_launch.m
@@ -1,12 +1,19 @@
 function app_launch()
-% scans the main directory for the app name and launches it
+    % scans the main directory for the app name and launches it
 
     a=dir('main/mfiles');
     a_IsDir = {a.isdir};
     a_Name  = {a.name};
-    a_Name_dir=a_Name(~cell2mat(a_IsDir )) ; 
-    a_Name_dir=a_Name_dir(:);
+    a_Name_dir  =   a_Name(~cell2mat(a_IsDir )) ;
+    a_Name_dir  =   a_Name_dir(:);
+    a_Name_dir  =   a_Name_dir{1};
+
+    % strip off extension
+    dotloc =  regexp(a_Name_dir,'[.]');
+    fName  =  a_Name_dir(1:dotloc-1);
     
-    eval(a_Name_dir);
+    %
+    disp(" Running l'App " )
+    eval(fName);
 
 end
