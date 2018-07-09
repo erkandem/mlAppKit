9ab451668a1d753abd5151fff1c5aca29e20a367
kan <kan@DESKTOP-N9B01HV.mshome.net>
2018-07-09 15:47:31 +0200

    deleted pre v0.5 files

--- functions/app_template_path_adder/app_template_path_adder.m
+++ functions/app_template_path_adder/app_template_path_adder.m
@@ -4,9 +4,7 @@ function app_template_path_adder()
    addpath(fullfile('functions','app_template_path_adder'));
    addpath(fullfile('functions','auto_child_registerer'));
    addpath(fullfile('functions','conversion_launcher'));
-   addpath(fullfile('functions','main_extractor'));
-   addpath(fullfile('functions','mlapp2classdef'));
-   addpath(fullfile('functions','mlapp2classdef_edit'));
+   addpath(fullfile('functions','helper_f'));
    addpath(fullfile('functions','panel_visibility'));
    addpath(fullfile('functions','path_adder_creator'));
    addpath(fullfile('launch','mfiles'));
