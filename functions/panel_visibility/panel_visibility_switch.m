1aa036ae98dc48860b927dbc6c885d7842a5cb23
pandakan <41024392+pandakan@users.noreply.github.com>
2018-08-25 04:56:18 +0200

    added Licence file and inserted markup for sphinx ( test)

--- functions/panel_visibility/panel_visibility_switch.m
+++ functions/panel_visibility/panel_visibility_switch.m
@@ -1,14 +1,22 @@
-
-
-
 function panel_visibility_switch(app,target_tag)
-
-% purpose:  regulates the visibility of panels/views in the main app. so
-%           instead of replicating on/off switches with each menu
-%           selection we would prefer to to know
-%               [ 1 ] which panel/view needs to be set to visible
-%               [ 2 ] -[1] implies which panels need to be turned off
-
+% regulates the visibility of panels/views in the main app. so
+% instead of replicating on/off switches with each menu
+% selection we would prefer to to know
+% [ 1 ] which panel/view needs to be set to visible
+% [ 2 ] -[ 1 ] implies which panels need to be turned off
+%
+%
+% :param app: an ``app`` MATLAB class to which contains all parts of the
+% applications. Important in this case are the handles to each panel which
+% can be comfortably access via ``app`` instead of collecting and passing
+% it manually to this function
+% :param target_tag: marks the tag of the tab which raised the function
+% call, as such, that panel is going to be made visible while every other
+% panel will be set to invisable
+% :returns ``void``: the function does not return
+%
+%
+%
 
 fn_app      = fieldnames(app );
 fn_app_type = cell(numel(fn_app),1);
