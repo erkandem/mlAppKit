448dea37cca30c2c6e4a3a21efc1b024fdf53fe9
erkandem <41024392+pandakan@users.noreply.github.com>
2018-09-09 09:25:29 +0200

    renamed buildTheApp to make - keep it simple

---
+++ make.m
@@ -0,0 +1,41 @@
+function make(pdir)
+% Actually a script in a function shell is going to "build" and run the app 
+
+%% Look at the  dependencies 
+    if nargin ==0
+        projectDir = pwd;
+    else 
+        currentdir = pdir;
+    end
+
+    addpath(fullfile('functions','building'));
+    addPath_creator()
+
+%% add the project files to MATLAB's <session> search  path
+% this function was auto created by "path_adder_creator"
+    addpath(fullfile('functions','auto_generated'));
+    builder_addPath();
+
+%%  prepare m-code extraction
+
+    conversion_func_generator();
+% creates the "conversion_launcher()" function.
+
+%% extract m-code
+% essentially a script turned into a funtion
+% runs the appdesigner to m file function 
+
+    conversion_launcher();
+%
+% :todo: compatibility to mlapp2classdef() ?
+%
+%% re-run the path search
+% in order to capture the location of the new m-files
+    addPath_creator()
+    builder_addPath();
+
+%% launch the app 
+% started from the obtained m-files not the .mlapp file
+    app_launch();
+ sprintf('%s','Aplication built successfully! ')
+end
