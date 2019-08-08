2b7e03cef5114a38519a8dbe08a68765e056f57a
kan <erkan.demiralay@web.de>
2019-08-08 13:11:37 +0200

    - add tests.m  - add the project logo

---
+++ tests/tests.m
@@ -0,0 +1,8 @@
+% define some test for your software to audit its quality they said
+%
+% When will it break ?
+% -> change environment
+% -> vary input
+% -> genrally, create elements of deviation and combine them
+%
+%
